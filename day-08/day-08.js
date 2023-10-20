9e27689081dd2400cf1321ae1a6e050754ee0b12
Adrian <adriann.dev@gmail.com>
2023-10-19 22:14:03 -0300

    Code update.

--- day-08/day-08.js
+++ day-08/day-08.js
@@ -1,6 +1,17 @@
 // APRENDENDO SOBRE FUNÇÃO (CONHECIMENTO BÁSICO DE ESTRUTRA)
 
-function somar (){
-    return "Você está rtornando isso"
+let nomeUssuario = prompt("Qual seu nome?")
+let idadeUsuario = Number(parseInt(prompt("Qual a sua idade?")))
 
+function idade (i){
+   if(i >= 18){
+    return "acesso permitido"
+
+   }else{
+    return "acesso negado"
+   }
 }
+
+let functionIdade = idade(idadeUsuario)
+
+console.log("Olá " + nomeUssuario + ", você tem " + idadeUsuario + " e por isso teve seu " + functionIdade + "!")
